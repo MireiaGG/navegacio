bffb3828c93dde6fc94650b0267302d067a403f8
mireia <mgarciag@elpuig.xeill.net>
2026-09-18 19:09:46 +0200

    m

---
+++ untitled/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        boolean salir = true;
+        while (!salir){
+            System.out.println("Bienvenido al menú principal elige una de las opciones:");
+            System.out.println("1.Gestión de usuarios");
+            System.out.println("2.Mirar estado del sistema");
+            System.out.println("0.salir");
+            int opc = sc.nextInt();
+
+        }
+    }
+}
